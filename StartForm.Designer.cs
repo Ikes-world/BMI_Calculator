3a3b693ce8b7f89ad8629f6fcada71d90296b059
Ikes-world <52172215+Ikes-world@users.noreply.github.com>
2019-07-26 23:33:44 -0400

    Created a Splash Screen

---
+++ StartForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Assignment5_BMI_Calculator
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.BMII_Intro = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.BMI_Timmer = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// BMII_Intro
+			// 
+			this.BMII_Intro.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BMII_Intro.Location = new System.Drawing.Point(12, 152);
+			this.BMII_Intro.Name = "BMII_Intro";
+			this.BMII_Intro.Size = new System.Drawing.Size(269, 46);
+			this.BMII_Intro.TabIndex = 0;
+			this.BMII_Intro.Text = "BMI CALCULATOR";
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(85, 198);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(125, 30);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Author: Isaac Kim";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(94, 217);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(125, 20);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "ID: 301015092";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(66, 237);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(170, 17);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Modification: 07/25/19";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(42, 254);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(215, 19);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Purpose to Calculate your BMI";
+			// 
+			// BMI_Timmer
+			// 
+			this.BMI_Timmer.Interval = 4500;
+			this.BMI_Timmer.Tick += new System.EventHandler(this.BMI_Timmer_Tick);
+			// 
+			// StartForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(304, 441);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.BMII_Intro);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.MaximizeBox = false;
+			this.Name = "StartForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "BMI Calculator";
+			this.Load += new System.EventHandler(this.StartForm_Load);
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Label BMII_Intro;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Timer BMI_Timmer;
+	}
+}
---
+++ StartForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5_BMI_Calculator
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+		private void StartForm_Load(object sender, EventArgs e)
+		{
+			BMI_Timmer.Enabled = true;
+		}
+
+		private void BMI_Timmer_Tick(object sender, EventArgs e)
+		{
+			BMI_Timmer.Enabled = false;
+
+			Program.bMI.Show();
+			this.Hide()
+				
+				;
+		}
+	}
+}
